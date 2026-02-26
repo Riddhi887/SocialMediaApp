7110657a427852c455dcf7312b0abe3e8d92dab6
Riddhi138 <147044631+Riddhi138@users.noreply.github.com>
2026-02-26 21:48:12 +0530

    Worked with Authentication tokens

--- twitter_server/app/user/resolvers.ts
+++ twitter_server/app/user/resolvers.ts
@@ -1,9 +1,20 @@
 //resolvers for all token
 //1st resolver : you'll get a token
-
+import axios, { Axios } from "axios";  //for api calling 
 const queries = {
     verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-        return token;
+        const googleToken = token;
+        //make url 
+        const googleOauthURL = new URL('https://oauth2.googleapis.com/tokeninfo')
+        //set the id_token ad google token in url
+        googleOauthURL.searchParams.set('id_token', googleToken)
+        
+        //make request to particular url 
+        const { data} = await axios.get(googleOauthURL.toString(), {
+            responseType: 'json'
+        })
+        console.log("User Data : " + data);
+        return "ok";
     },
 };
 
